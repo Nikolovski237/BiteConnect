2c470b99692cedfee73b10c5e68f8f5ea1936890
mn29983 <mn29983@seeu.edu.mk>
2024-01-11 15:36:15 +0100

    Working on Frontend

--- resources/views/food_types.blade.php
+++ resources/views/food_types.blade.php
@@ -1,12 +1,12 @@
-<h3>Food categories</h3>
 
 <div id="foodSlider" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
+        <h3>Food categories</h3>
         <div class="carousel-item active">
             <div class="card-deck">
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/Burger.jpg') }}" class="card-img-top custom-card-img" alt="Burger">
+                        <img src="{{ asset('images/Food/Burger.jpg') }}" class="card-img-top custom-card-img" alt="Burger" loading="lazy">
                         <div class="card-body">
                             <h5 class="card-title">Burger</h5>
                         </div>
@@ -14,7 +14,7 @@
                 </div>
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/Pizza.jpg') }}" class="card-img-top custom-card-img" alt="Pizza">
+                        <img src="{{ asset('images/Food/Pizza.jpg') }}" class="card-img-top custom-card-img" alt="Pizza" loading="lazy">
                         <div class="card-body">
                             <h5 class="card-title">Pizza</h5>
                         </div>
@@ -22,7 +22,7 @@
                 </div>
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/Pasta.jpg') }}" class="card-img-top custom-card-img" alt="Pasta">
+                        <img src="{{ asset('images/Food/Pasta.jpg') }}" class="card-img-top custom-card-img" alt="Pasta" loading="lazy">
                         <div class="card-body">
                             <h5 class="card-title">Pasta</h5>
                         </div>
@@ -30,7 +30,7 @@
                 </div>
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/Salad.jpg') }}" class="card-img-top custom-card-img" alt="Salad">
+                        <img src="{{ asset('images/Food/Salad.jpg') }}" class="card-img-top custom-card-img" alt="Salad" loading="lazy">
                         <div class="card-body">
                             <h5 class="card-title">Salad</h5>
                         </div>
@@ -38,7 +38,7 @@
                 </div>
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/Soup.jpg') }}" class="card-img-top custom-card-img" alt="Soup">
+                        <img src="{{ asset('images/Food/Soup.jpg') }}" class="card-img-top custom-card-img" alt="Soup" loading="lazy">
                         <div class="card-body">
                             <h5 class="card-title">Soup</h5>
                         </div>
@@ -51,7 +51,7 @@
             <div class="card-deck">
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/Sandwich.jpg') }}" class="card-img-top custom-card-img" alt="Sandwich">
+                        <img src="{{ asset('images/Food/Sandwich.jpg') }}" class="card-img-top custom-card-img" alt="Sandwich" loading="lazy">
                         <div class="card-body">
                             <h5 class="card-title">Sandwich</h5>
                         </div>
@@ -59,7 +59,7 @@
                 </div>
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/Wrap.jpg') }}" class="card-img-top custom-card-img" alt="Wrap">
+                        <img src="{{ asset('images/Food/Wrap.jpg') }}" class="card-img-top custom-card-img" alt="Wrap" loading="lazy"> 
                         <div class="card-body">
                             <h5 class="card-title">Wraps</h5>
                         </div>
@@ -67,7 +67,7 @@
                 </div>
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/Pancake.jpg') }}" class="card-img-top custom-card-img" alt="Desert">
+                        <img src="{{ asset('images/Food/Pancake.jpg') }}" class="card-img-top custom-card-img" alt="Desert" loading="lazy">
                         <div class="card-body">
                             <h5 class="card-title">Desert</h5>
                         </div>
@@ -75,7 +75,7 @@
                 </div>
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/Fish.png') }}" class="card-img-top custom-card-img" alt="Fish">
+                        <img src="{{ asset('images/Food/Fish.jpg') }}" class="card-img-top custom-card-img" alt="Fish" loading="lazy">
                         <div class="card-body">
                             <h5 class="card-title">Fish</h5>
                         </div>
@@ -83,7 +83,7 @@
                 </div>
                 <div class="card">
                     <a href="#">
-                        <img src="{{ asset('images/Food/BBQ.jpg') }}" class="card-img-top custom-card-img" alt="BBQ">
+                        <img src="{{ asset('images/Food/BBQ.jpg') }}" class="card-img-top custom-card-img" alt="BBQ" loading="lazy">
                         <div class="card-body">
                             <h5 class="card-title">BBQ</h5>
                         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,70 +5,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bite Connect</title>
     <!-- Add your CSS stylesheets or CDN links here -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/custom.css') }}">
     
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">Bite Connect</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{ url('/') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('restaurants.index') }}">Restaurants</a>
-                    </li>
-                    @auth
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('cart.show') }}">Cart</a>
-                    </li>
-                    @endauth
-                </ul>
-                <ul class="navbar-nav ml-auto">
-                    @auth
-                    @if(auth()->user()->isMasterAdmin())
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('users.index') }}">View Users</a>
-                        </li>
-                    @endif
-                    @endauth
-                    @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
-                    </li>
-                    @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                        <span> Welcome, {{ Auth::user()->name }}</span>
-                    </li>
-                    @endguest
-                </ul>
-            </div>
+        <a class="navbar-brand" href="{{ url('/') }}">Bite Connect</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ url('/') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('restaurants.index') }}">Restaurants</a>
+                </li>
+                @auth
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('cart.show') }}">Cart</a>
+                </li>
+                @endauth
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                @auth
+                @if(auth()->user()->isMasterAdmin())
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('users.index') }}">View Users</a>
+                </li>
+                @endif
+                @endauth
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    <span> Welcome, {{ Auth::user()->name }}</span>
+                </li>
+                @endguest
+            </ul>
         </div>
     </nav>
+        <div class="container mt-4 container-slightly-wider" id="foodTypesContainer">
+            @yield('content')
+        </div>
 
-    <div class="container mt-4 container-slightly-wider" id="foodTypesContainer">
-        @yield('content')
-    </div>
 
-    <!-- Add your JavaScript scripts  -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- Add your JavaScript scripts  -->
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
     <script>
         $(document).ready(function() {
