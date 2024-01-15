ae63f232c20bc78a11a6f7e5ddf78bc9ad8fd0d6
mn29983 <mn29983@seeu.edu.mk>
2024-01-15 15:51:06 +0100

    Styling the website

--- resources/views/menus/show.blade.php
+++ resources/views/menus/show.blade.php
@@ -22,15 +22,23 @@
                 @foreach($items as $menuItem)
                 <div class="col-md-4 mb-4">
                     <div class="card">
-                        <img src="{{ asset($restaurant->image) }}" class="card-img-top" style="height: 250px; object-fit: cover;" alt="{{ $restaurant->name }}">
+                        <img src="{{ asset($restaurant->image) }}" class="card-img-top" alt="{{ $restaurant->name }}">
                         <div class="card-body">
                             <h5 class="card-title">{{ $menuItem->name }}</h5>
                             <p class="card-text">{{ $menuItem->description }}</p>
                             <p class="card-text">Price: {{ $menuItem->price }}</p>
                             @auth
-                                <form method="POST" action="{{ route('cart.addToCart', $menuItem->id) }}">
-                                    @csrf
-                                    <button type="submit" class="btn btn-primary">Add to Cart</button>
+                            <form method="POST" action="{{ route('cart.addToCart', $menuItem->id) }}">
+                                @csrf
+                                <button type="submit" class="AddButton">
+                                    <div tabindex="0" class="plusButton">
+                                    <svg class="plusIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                                        <g mask="url(#mask0_21_345)">
+                                        <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z"></path>
+                                        </g>
+                                    </svg>
+                                    </div>
+                                </button>
                                 </form>
                             @endauth
                         </div>
