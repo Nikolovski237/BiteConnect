719f29f5abf1647295b890c88534ec7eedcc73a7
Martin <mn29983@seeu.edu.mk>
2024-02-09 15:51:49 +0100

    Update on last push

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
 
 
-// Authentication Routes
+// Account Routes
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
